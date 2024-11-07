1b11e02baf41b10046314e40411e6e30f38176b0
Daniel Lima <dsl.daniel.santos.lima@gmail.com>
2024-11-07 00:21:49 -0300

    Exercícios de Java

--- src/modulo07_lacosecondicionais/AtividadePratica2Exercicio1.java
+++ src/modulo07_lacosecondicionais/AtividadePratica2Exercicio1.java
@@ -1,10 +1,41 @@
 package modulo07_lacosecondicionais;
 
+import java.util.Scanner;
+
 public class AtividadePratica2Exercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Atividade Prática2
+		//Laços e Condicionais
+		//Exercício 1
+		//Faça um algoritmo em Java que leia
+		// 3 valores inteiros A, B e C e
+		// imprima na tela se a soma de A + B é maior,
+		// menor ou igual a C.
+		
+		int a, b, c;
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("--Programa para comparação entre valores--");
+		System.out.println("\n Digite o 1º Número (A): ");
+		a = leia.nextInt();
+								
+		System.out.println("\n Digite o 2º Número (B): ");
+		b = leia.nextInt();
+		
+		System.out.println("\n Digite o 3º Número (C): ");
+		c = leia.nextInt();
+		
+		if((a + b) > c) { 
+			System.out.println("A soma de A + B é maior do que C");
+		} else if ((a + b) < c){
+			System.out.println("A soma de A + B é menor do que C");			
+		} else {
+			System.out.println("A soma de A + B é igual a C");
+		}
+		
+		leia.close();
 	}
 
 }
